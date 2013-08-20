c329de160c4a4b7455101ed4dbcab2cb3c21c675
Hendrik Molter <hendrik.molter@ipa.fraunhofer.de>
2013-08-20 14:34:46 +0200

    small changes to make it build

--- cob_camera_sensors/common/include/cob_camera_sensors/VirtualColorCam.h
+++ cob_camera_sensors/common/include/cob_camera_sensors/VirtualColorCam.h
@@ -136,7 +136,6 @@ class __DLL_LIBCAMERASENSORS__ VirtualColorCam : public AbstractColorCamera
 		/// @param path The camera path
 		/// @return Return code
 		unsigned long SetPathToImages(std::string path);
-		unsigned long ResetImages();
 
 };
 
